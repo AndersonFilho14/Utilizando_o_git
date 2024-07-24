dd32d4e08d9f069c11cf1c97915ed4b24f800205
Anderson Filho <anderson.filho@parvi.local>
2024-07-24 10:05:46 -0300

    Fiz os comandos basicos de branchs

---
+++ freeCodeCamp.py
@@ -0,0 +1,15 @@
+# Fazendo o projeto do freeCodeCamp. Mas antes já iniciei o git flow e começar uma feature
+    Um erro que era previsto, mas eu não previ. Criei uma feature, mas como eu vou trabalhar com branch,
+    A feature não sera capaz de ver as alterações 
+#2. Git branch
+    Vou criar uma nova branch e depois enviar ela para o repositorio remoto
+    para criar usa o comando 'git branch <nome-da-branch>'
+    E para enviar para o repositorio, 'git push -u <local-remoto> <nome-da-branch>'     Obs, na parte de envira o commit o git recomendou usar 'git push --set-upstream origin nome_da_branch'
+    Para ver a branch sou usar 'git branch'
+    E para excluíla 'git branch -d ,nome da branch>'
+
+Testando se a branch 'nome_da_branch' vai ser commitada corretamente, 
+só sera possivel ver essa parte nessa branch
+
+
+
