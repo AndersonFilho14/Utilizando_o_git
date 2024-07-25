791993befc01411caa39b3fb8a2d4a0064498167
Anderson Filho <anderson.filho@parvi.local>
2024-07-25 15:59:40 -0300

    remote

--- comandos.py
+++ comandos.py
@@ -1,5 +1,9 @@
 # git log --graph --oneline --decorate
     usado pra ficar visivel as branch do git, se usar o  git log --graph já cumpre a missão. Podendo filtrar o historico.
 
-#
+#git remote
+    Serve pra conectar o repositorio local com o remoto
+    'git add origin <link>'
+    o nome 'origin' é um nome defaut,  podendo ser alterado pra um nome de sua escolha.Mas não faça merda, so segue o fluxo
+    'git remote -v' serve pra pegar o endereço
 
