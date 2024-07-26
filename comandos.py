340164bd84a676c16ec5caaba431614d906615b9
Anderson Filho <anderson.filho@parvi.local>
2024-07-26 15:04:53 -0300

    cherry pick

--- comandos.py
+++ comandos.py
@@ -7,3 +7,7 @@
     o nome 'origin' é um nome defaut,  podendo ser alterado pra um nome de sua escolha.Mas não faça merda, so segue o fluxo
     'git remote -v' serve pra pegar o endereço
 
+#git cherry-pick
+    cherry-pick é uma escolher a dedo, pegando um commit expecifico em outra branch e trazendo para sua
+    Correção de Bugs: Você pode querer aplicar um commit que corrige um bug em várias branches diferentes.
+    'git cherry-pick <hash do commit>'
